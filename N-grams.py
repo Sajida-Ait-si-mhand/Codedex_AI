2c19fc54aded7d7aff08e66a182a4d6c4b48bded
Sajida Ait si mhand <saait-si@c4r8p10.1337.ma>
2024-12-19 06:40:04 +0100

    some colors

--- N-grams.py
+++ N-grams.py
@@ -6,4 +6,5 @@ from nltk.util import ngrams
 sentence = "I am a princess"
 token = word_tokenize(sentence)
 grams = list(ngrams(token, 2))
-print("grams:", grams)
+print("\033[92mngrams:\033[0m", "\033[95m", grams, "\033[0m")
+# print("\033[92mngrams:\033[0m", "\033[95m", grams, "\033[0m")
